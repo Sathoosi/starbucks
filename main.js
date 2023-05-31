6fe3b553175be4713bef5b564d0acf478759210d
Walid Sathosi <70620951+Sathoosi@users.noreply.github.com>
2023-05-31 21:36:52 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function imgSlider(anything) {
+    document.querySelector('.starbucks').src = anything;
+}
+
+function colors(color) {
+    const circle = document.querySelectorAll('.color');
+    circle.style.background = color;
+}
